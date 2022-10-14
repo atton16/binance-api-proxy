f23773109330390c793b664b56b0a56fd679d529
Attawit Kittikrairit <atton16@gmail.com>
2022-10-14 17:32:05 +0700

    refactor: clean up build process

--- lambdaBuild.sh
+++ lambdaBuild.sh
@@ -2,6 +2,7 @@ rm -rf dist
 mkdir -p dist
 cp *.js dist/
 cp package*.json dist/
-(cd dist && npm i --production)
+(cd dist && NODE_ENV=production npm install)
+rm -rf function.zip
 (cd dist && zip -r ../function.zip .)
 rm -rf dist

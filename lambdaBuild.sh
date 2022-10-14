ad3cead392035a713e271ad724083c6d62e73ada
Attawit Kittikrairit <atton16@gmail.com>
2022-10-14 17:11:40 +0700

    refactor: rename package-lambda.json to package-lambdaBuild.json

--- lambdaBuild.sh
+++ lambdaBuild.sh
@@ -1,7 +1,7 @@
 rm -rf dist
 mkdir -p dist
 cp index.js dist/
-cp package-lambda.json dist/package.json
+cp package-lambdaBuild.json dist/package.json
 (cd dist && npm i --only-production)
 (cd dist && zip -r ../function.zip .)
 rm -rf dist
